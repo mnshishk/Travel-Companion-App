ee9c6161a1df51d2b231f1e5f6bd2e5068985d40
mnnuscript <mnshishk@buffalo.edu>
2020-09-24 18:31:12 -0400

    Revert "adding main.js"
    This reverts commit 9f1bc6d912bcebaf5f6250332f7a5f7f18d8c4e3.

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
-let toggleLeftBar = function(){
-	const sidebarLeft = document.querySelector(".left-panel");
-	const sidebarLeft_ul = document.querySelector(".left-panel ul");
-	const sidebarLeft_li = document.querySelectorAll(".left-panel li");
-	sidebarLeft.addEventListener("click")
-}
+
