98758aeebc81ecefd07b5229cceb953141723871
mnnuscript <mnshishk@buffalo.edu>
2020-09-24 18:14:11 -0400

    made the map appear on the website and ready to receive data to display

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-let toggleMenuClosed = true;
-
-let toggle = function(){
-	let getSidebarLeft = document.querySelector();
+let toggleLeftBar = function(){
+	const sidebarLeft = document.querySelector(".left-panel");
+	const sidebarLeft_ul = document.querySelector(".left-panel ul");
+	const sidebarLeft_li = document.querySelectorAll(".left-panel li");
+	sidebarLeft.addEventListener("click")
 }
