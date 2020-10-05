78e52f4d1dbe6c974edee3af220597467c0ef654
kevinchen0135 <43007580+kevinchen0135@users.noreply.github.com>
2020-10-05 00:25:44 -0400

    #38 code fixes

--- map.js
+++ map.js
@@ -1,6 +1,10 @@
+
 function initMap(){
 	var map = new google.maps.Map(document.getElementById("map"), {
 		zoom: 8,
-		center: {lat:42.880230,lng:-78.878738}
+		center: {lat:42.880230,lng:-78.878738},
+		mapTypeControl: false,
+		fullscreenControl: false,
+		zoomControl: false
 	});
 }
