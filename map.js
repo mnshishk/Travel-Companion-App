de7e8975b47a2defced5419f199db506fb885d5c
casey8989 <caseydol@buffalo.edu>
2020-10-11 14:19:59 -0400

    Added the ability to have routes displayed on the map using the embeded map api
    USing the embedded routing library, I added a css panel that alows the user to select from a list of options to display a route between them. The next step is makingthis a general input box rather then a preselected place

--- map.js
+++ map.js
@@ -1,9 +1,47 @@
+<<<<<<< Updated upstream
 function initMap(){
 	var map = new google.maps.Map(document.getElementById("map"), {
 		zoom: 8,
 		center: {lat:42.880230,lng:-78.878738},
+=======
+function initMap() {
+  const directionsService = new google.maps.DirectionsService();
+  const directionsRenderer = new google.maps.DirectionsRenderer();
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 7,
+    center: { lat: 42.88, lng: -78.87 },
+>>>>>>> Stashed changes
 		mapTypeControl: false,
 		fullscreenControl: false,
 		zoomControl: false
-	});
+
+  });
+  directionsRenderer.setMap(map);
+
+  const onChangeHandler = function () {
+    calculateAndDisplayRoute(directionsService, directionsRenderer);
+  };
+  document.getElementById("start").addEventListener("change", onChangeHandler);
+  document.getElementById("end").addEventListener("change", onChangeHandler);
+}
+
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+  directionsService.route(
+    {
+      origin: {
+        query: document.getElementById("start").value,
+      },
+      destination: {
+        query: document.getElementById("end").value,
+      },
+      travelMode: google.maps.TravelMode.DRIVING,
+    },
+    (response, status) => {
+      if (status === "OK") {
+        directionsRenderer.setDirections(response);
+      } else {
+        window.alert("Directions request failed due to " + status);
+      }
+    }
+  );
 }
