59315304ffbe3d21df96e6551fa1d2c9bc6b7549
kevinchen0135 <43007580+kevinchen0135@users.noreply.github.com>
2020-10-19 00:02:57 -0400

    Dev -> Master merge

--- map.js
+++ map.js
@@ -1,3 +1,13 @@
+//Global variabvles that take in the position of the user and
+//the geocode result of the search bar so that they can be used in calculateAndDisplayRoute funtion
+var globalOrigin;
+var globalEnd;
+var places;
+var infoWindow;
+var markers = [];
+var currentLoc;
+var tempVar;
+var map;
 //create a marker for the map
 function setMarker(pos, map){
 	var marker = new google.maps.Marker({
@@ -19,13 +29,16 @@ function getUserLoc(geocoder, map){
         navigator.geolocation.getCurrentPosition(function(position){
         	// got the lattitude and longitude
         	var latLng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude)
-            map.setCenter(latLng);
+					globalOrigin = new google.maps.LatLng(position.coords.latitude,position.coords.longitude)
+					tempVar = { lat: parseFloat(position.coords.latitude), lng: parseFloat(position.coords.longitude)};
+					  //map.setCenter(latLng);
             geocoder.geocode({ location: latLng }, (results, status) => {
 			    if (status === "OK") {
 			      if (results[0]) {
 			        map.setZoom(11);
 			        map.setCenter(latLng);
             		var marker = setMarker(latLng, map, "You are here");
+								holder = marker;
 			        infoWin.setContent(results[0].formatted_address);
 			        infoWin.open(map, marker);
 			      } else {
@@ -49,12 +62,9 @@ function geocodeAddress(geocoder, resultsMap) {
 	const address = document.getElementById("address").value;
 	geocoder.geocode({ address: address }, (results, status) => {
 	  if (status === "OK") {
-		resultsMap.setCenter(results[0].geometry.location);
-		resultsMap.setZoom(15);
-		new google.maps.Marker({
-		  map: resultsMap,
-		  position: results[0].geometry.location,
-		});
+		//resultsMap.setCenter(results[0].geometry.location);
+		//resultsMap.setZoom(15);
+		globalEnd = results[0].geometry.location;
 	  } else {
 		alert("Geocode was not successful for the following reason: " + status);
 	  }
@@ -63,28 +73,24 @@ function geocodeAddress(geocoder, resultsMap) {
 
 
 function calculateAndDisplayRoute(directionsService, directionsRenderer) {
-  directionsService.route(
-    {
-      origin: {
-        query: document.getElementById("start").value,
-      },
-      destination: {
-        query: document.getElementById("end").value,
-      },
-      travelMode: google.maps.TravelMode.DRIVING,
-    },
-    (response, status) => {
-      if (status === "OK") {
-        directionsRenderer.setDirections(response);
-      } else {
-        window.alert("Directions request failed due to " + status);
-      }
-    }
-  );
+ var start = document.getElementById('start').value;
+ var end = document.getElementById('end').value;
+ var request = {
+	 origin: globalOrigin,
+	 destination: globalEnd,
+	 travelMode: 'DRIVING'
+ };
+ directionsService.route(request, function(result, status) {
+	 if (status == 'OK') {
+		 directionsRenderer.setDirections(result);
+	 } else {
+	 alert("Route was not successful for the following reason: " + status);
+	 }
+ });
 }
 
 function initMap(){
-	var map;
+//	var map;
 	const directionsService = new google.maps.DirectionsService();
 	const directionsRenderer = new google.maps.DirectionsRenderer();
 
@@ -96,6 +102,7 @@ function initMap(){
 		zoomControl: false
 	}
 	geocoder = new google.maps.Geocoder();
+//	tempVar = new google.maps.Map(document.getElementById("map"), mapOptions);
 	map = new google.maps.Map(document.getElementById("map"), mapOptions);
 	getUserLoc(geocoder,map)
 
@@ -108,7 +115,132 @@ function initMap(){
 	const onChangeHandler = function () {
 		calculateAndDisplayRoute(directionsService, directionsRenderer);
 	};
-	document.getElementById("start").addEventListener("change", onChangeHandler);
+	document.getElementById("submit").addEventListener("click", onChangeHandler);
 	document.getElementById("end").addEventListener("change", onChangeHandler);
+	//const service = new google.maps.places.PlacesService(map);
+  let getNextPage;
+  const moreButton = document.getElementById("more");
+  moreButton.onclick = function () {
+    moreButton.disabled = true;
+
+    if (getNextPage) {
+      getNextPage();
+    }
+  };
+
+	document.getElementById("Hotels").addEventListener("click", () => {
+	  searchLodging();
+	});
+	document.getElementById("Entertainment").addEventListener("click", () => {
+	  searchEntertainment();
+	});
+	document.getElementById("Gas").addEventListener("click", () => {
+		searchGas();
+	  });
 }
 
+function searchLodging(){
+	const service = new google.maps.places.PlacesService(map);
+	const pyrmont = { lat: 42.8864, lng: -78.8784};
+  service.nearbySearch(
+    { location: globalOrigin, radius: 3280, type: "lodgings" },
+    (results, status, pagination) => {
+      if (status !== "OK"){
+				return;
+			}
+      createMarkers(results, map);
+      moreButton.disabled = !pagination.hasNextPage;
+
+      if (pagination.hasNextPage) {
+        getNextPage = pagination.nextPage;
+      }
+    }
+  );
+}
+
+function searchEntertainment(){
+	const service = new google.maps.places.PlacesService(map);
+	const pyrmont = { lat: 42.8864, lng: -78.8784};
+  service.nearbySearch(
+    { location: globalOrigin, radius: 3280, type: "bar" },
+    (results, status, pagination) => {
+      if (status !== "OK"){
+				return;
+			}
+      createMarkers(results, map);
+      moreButton.disabled = !pagination.hasNextPage;
+
+      if (pagination.hasNextPage) {
+        getNextPage = pagination.nextPage;
+      }
+    }
+  );
+}
+function searchGas(){
+	const service = new google.maps.places.PlacesService(map);
+	const pyrmont = { lat: 42.8864, lng: -78.8784};
+	
+  service.nearbySearch(
+    { location: globalOrigin, radius: 1000, type: "gas_station" },
+    (results, status, pagination) => {
+      if (status !== "OK"){
+				return;
+			}
+      createMarkers(results, map);
+      moreButton.disabled = !pagination.hasNextPage;
+
+      if (pagination.hasNextPage) {
+        getNextPage = pagination.nextPage;
+      }
+    }
+  );
+}
+// function searchLandmark(){
+// console.log('shout');
+// 	const service = new google.maps.places.PlacesService(map);
+// 	const pyrmont = { lat: 42.8864, lng: -78.8784};
+// 	console.log(tempVar);
+//   service.nearbySearch(
+//     { location: pyrmont, radius: 3280, type: "points_of_interest" },
+//     (results, status, pagination) => {
+// 			console.log('stug');
+//       if (status !== "OK"){
+// 				console.log(status);
+// 				return;
+// 			}
+//       createMarkers(results, map);
+//       moreButton.disabled = !pagination.hasNextPage;
+//
+//       if (pagination.hasNextPage) {
+//         getNextPage = pagination.nextPage;
+//       }
+//     }
+//   );
+// 	console.log("landmark");
+// }
+
+function createMarkers(places, map) {
+  const bounds = new google.maps.LatLngBounds();
+  const placesList = document.getElementById("places");
+
+  for (let i = 0, place; (place = places[i]); i++) {
+    const image = {
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25),
+    };
+    new google.maps.Marker({
+      map,
+      icon: image,
+      title: place.name,
+      position: place.geometry.location,
+    });
+    const li = document.createElement("li");
+    li.textContent = place.name;
+    placesList.appendChild(li);
+    bounds.extend(place.geometry.location);
+  }
+  map.fitBounds(bounds);
+}
