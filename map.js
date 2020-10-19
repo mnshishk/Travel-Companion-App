b13472ff8b78dcb4537460b28e14edfd8478eff0
mnnuscript <mnshishk@buffalo.edu>
2020-10-19 04:31:20 -0400

    Minor fixes for the UI of the previous tasks.

--- map.js
+++ map.js
@@ -244,3 +244,13 @@ function createMarkers(places, map) {
   }
   map.fitBounds(bounds);
 }
+
+function initMap(){
+	var map = new google.maps.Map(document.getElementById("map"), {
+		zoom: 8,
+		center: {lat:42.880230,lng:-78.878738},
+		mapTypeControl: false,
+		fullscreenControl: false,
+		zoomControl: false
+	});
+}
